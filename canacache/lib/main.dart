98a3f63c6400e00100f66d5ea65328b843364025
object-Object <object@objectobject.ca>
2022-12-03 16:15:43 -0500

    Lock app to portrait mode (fixes #20)

--- canacache/lib/main.dart
+++ canacache/lib/main.dart
@@ -1,4 +1,14 @@
 import "package:canacache/features/app/view/app.dart";
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 
-void main() => runApp(const CanaApp());
+void main() async {
+  // lock to portrait mode
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(const CanaApp());
+}
