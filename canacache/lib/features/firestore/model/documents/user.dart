41f1ad2dc8b0bb8b1ca34d92aa58a6bd53084a6a
object-Object <object@objectobject.ca>
2022-12-04 15:11:52 -0500

    Remove unneeded constructor

--- canacache/lib/features/firestore/model/documents/user.dart
+++ canacache/lib/features/firestore/model/documents/user.dart
@@ -1,6 +1,5 @@
 import "package:canacache/features/firestore/model/collections/caches.dart";
 import "package:canacache/features/firestore/model/collections/user_items.dart";
-import "package:canacache/features/firestore/model/collections/users.dart";
 import "package:canacache/features/firestore/model/document_model.dart";
 import "package:canacache/features/firestore/model/documents/cache.dart";
 import "package:cloud_firestore/cloud_firestore.dart";
@@ -9,11 +8,6 @@ import "package:cloud_firestore/cloud_firestore.dart";
 class CanaUser extends DocumentModel<CanaUser> {
   // fields
   List<DocumentReference<Cache>> visitedCaches;
-
-  CanaUser({
-    required String id,
-    this.visitedCaches = const [],
-  }) : super(Users().getDocumentReference(id));
 
   CanaUser.withRef({
     required DocumentReference<CanaUser> ref,
