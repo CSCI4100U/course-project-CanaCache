a2684ac7e0a1105867c751a4f032be7f4fde635a
Imran M <imran.mustafa@ontariotechu.net>
2022-12-05 16:53:25 -0500

    Migrated list to Data table

--- canacache/lib/features/firestore/view/cache_list.dart
+++ canacache/lib/features/firestore/view/cache_list.dart
@@ -6,6 +6,7 @@ import "package:canacache/features/firestore/model/documents/cache.dart";
 import "package:canacache/features/firestore/view/item_list.dart";
 import "package:flutter/material.dart";
 
+
 // this is in its own widget so that opening a cache doesn't refresh the stream
 // which was causing a loading indicator to briefly flicker
 class CacheList extends StatefulWidget {
@@ -20,48 +21,83 @@ class CacheList extends StatefulWidget {
 class CacheListState extends ViewState<CacheList, CacheListController> {
   CacheListState() : super(CacheListController());
 
+  bool sortAscending = false;
+  int sortIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.caches.length,
-      itemBuilder: (context, index) {
-        final cache = widget.caches[index];
-        final isSelected = con.selectedIndex == index;
+    return DataTable(columns: <DataColumn> [
+      DataColumn(
+          label: const Text("Title"),
+          numeric: false,
+          onSort: (int index, _) {
+            setState(() {
+              sortIndex = index;
+              if (sortAscending == true) {
+                sortAscending = false;
+                // sort the product list in Ascending, order by Price
+                widget.caches.sort((A, B) =>
+                    B.name.compareTo(A.name),);
+              } else {
+                sortAscending = true;
+                // sort the product list in Descending, order by Price
+                widget.caches.sort((A, B) =>
+                    A.name.compareTo(B.name),);
+              }
+            });
+          },
+      ),
+      DataColumn(
+        label: const Text("Location"),
+        numeric: true,
+        onSort: (int index, _) {
+          setState(() {
+            sortIndex = index;
+            if (sortAscending == true) {
+              // 43.9457895491046, -78.89677587312467
+              sortAscending = false;
+              // sort the product list in Ascending, order by Price
+                widget.caches.sort((A, B) {
+                  double aLongDiff = 43.9457895491046 - A.position.longitude;
+                  double bLongDiff = 43.9457895491046 - B.position.longitude;
 
-        final tile = ListTile(
-          title: Text(cache.name),
-          subtitle: Text(cache.position.toLatLng()),
-          onTap: () => con.onTap(index),
-          trailing: isSelected
-              ? const Icon(Icons.arrow_drop_down)
-              : const Icon(Icons.arrow_right),
-        );
+                  double aLatDiff = -78.89677587312467 - A.position.latitude;
+                  double bLatDiff = -78.89677587312467 - B.position.latitude;
 
-        if (!isSelected) return tile;
+                  double aDiff = aLongDiff.abs() + aLatDiff.abs();
+                  double bDiff = bLongDiff.abs() + bLatDiff.abs();
 
-        return Column(
-          children: [
-            tile,
-            StreamBuilder(
-              stream: CacheItems(cache.ref).streamObjects(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  Error.throwWithStackTrace(
-                    snapshot.error!,
-                    snapshot.stackTrace!,
-                  );
-                }
+                  return aDiff > bDiff ? 0 : 1;
+                });
+              } else {
+              sortAscending = true;
+              // sort the product list in Descending, order by Price
+              widget.caches.sort((A, B) {
+                double aLongDiff = 43.9457895491046 - A.position.longitude;
+                double bLongDiff = 43.9457895491046 - B.position.longitude;
 
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const Center(child: LinearProgressIndicator());
-                }
+                double aLatDiff = -78.89677587312467 - A.position.latitude;
+                double bLatDiff = -78.89677587312467 - B.position.latitude;
 
-                return ItemList(items: snapshot.data!);
-              },
+                double aDiff = aLongDiff.abs() + aLatDiff.abs();
+                double bDiff = bLongDiff.abs() + bLatDiff.abs();
+
+                return aDiff > bDiff ? 1 : 0;
+              }
+              );
+              }
+          });
+        },
+      ),
+    ],
+      rows: List<DataRow>.generate(
+        widget.caches.length,
+            (index) => DataRow(cells: [
+              DataCell(Text(widget.caches[index].name)),
+              DataCell(Text(widget.caches[index].position.toLatLng())),
+            ],
             ),
-          ],
-        );
-      },
+      ),
     );
   }
 }
